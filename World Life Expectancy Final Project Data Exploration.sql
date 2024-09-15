d72e5f7c369440b914e85580c29fc5fb295c1073
Rishi Prasad <131681113+rishiprasad24@users.noreply.github.com>
2024-09-15 11:48:49 -0400

    Add files via upload

---
+++ World Life Expectancy Final Project Data Exploration.sql
@@ -0,0 +1,68 @@
+
+# World Life Expectancy Final Project (Exploratory Data Analysis) 
+
+SELECT *
+FROM world_life_expectancy;
+
+
+SELECT Country, 
+MIN(`Life expectancy`),
+MAX(`Life expectancy`),
+ROUND(MAX(`Life expectancy`) - MIN(`Life expectancy`), 1) AS Life_Increase_15_Years
+FROM world_life_expectancy 
+GROUP BY Country
+HAVING MIN(`Life Expectancy`) <> 0
+AND MAX(`Life Expectancy`) <> 0
+ORDER BY Life_Increase_15_Years DESC; 
+
+SELECT YEAR, ROUND(AVG(`Life expectancy`),2)
+FROM world_life_expectancy
+WHERE `Life expectancy` <> 0 
+AND `Life expectancy` <> 0
+GROUP BY Year
+ORDER BY Year; 
+
+SELECT *
+FROM world_life_expectancy;
+
+SELECT Country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(GDP),1) AS GDP
+FROM world_life_expectancy
+GROUP BY Country
+HAVING Life_Exp > 0
+AND GDP > 0
+ORDER BY GDP DESC;  
+
+SELECT 
+SUM(CASE WHEN GDP >= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
+AVG(CASE WHEN GDP >= 1500 THEN `Life expectancy` ELSE NULL END) High_GDP__Life_Expectancy,
+SUM(CASE WHEN GDP <= 1500 THEN 1 ELSE 0 END) High_GDP_Count,
+AVG(CASE WHEN GDP <= 1500 THEN `Life expectancy` ELSE NULL END) High_GDP__Life_Expectancy
+FROM world_life_expectancy; 
+
+
+SELECT Status, ROUND(AVG(`Life expectancy`),1)
+FROM world_life_expectancy
+GROUP BY Status;
+
+SELECT Status, COUNT(DISTINCT Country), ROUND(AVG(`Life expectancy`),1)
+FROM world_life_expectancy
+GROUP BY Status; 
+
+
+SELECT Country, ROUND(AVG(`Life expectancy`),1) AS Life_Exp, ROUND(AVG(BMI),1) AS BMI
+FROM world_life_expectancy
+GROUP BY Country
+HAVING Life_Exp > 0
+AND BMI > 0
+ORDER BY BMI ASC;
+
+SELECT *
+FROM world_life_expectancy;
+
+SELECT Country, 
+Year, 
+`Life expectancy`,
+`Adult Mortality`,
+SUM(`Adult Mortality`) OVER (PARTITION BY Country ORDER BY YEAR) AS Rolling_Total 
+FROM world_life_expectancy
+WHERE Country LIKE '%United%'; 
